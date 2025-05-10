13c865cbf79ef22d397da4281debfc464aadba08
Ars-Thong <v.t.nguyen@ar-system.co.jp>
2025-05-10 17:55:03 +0900

    resolve conflix

--- app/config/eccube/bundles.php
+++ app/config/eccube/bundles.php
@@ -28,4 +28,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Eccube\EccubeBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
