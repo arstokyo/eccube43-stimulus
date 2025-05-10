83290bd2e6ac40bdc039918ef0dacb08a350c507
Ars-Thong <v.t.nguyen@ar-system.co.jp>
2025-05-10 21:13:37 +0900

    Add Symfony UX Stimulus Bundle for enhanced frontend interactivity

--- app/config/eccube/bundles.php
+++ app/config/eccube/bundles.php
@@ -29,4 +29,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Eccube\EccubeBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
