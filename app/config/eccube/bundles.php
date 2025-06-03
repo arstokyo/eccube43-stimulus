1cb8b0de66ebd110e70949d55b578de570869630
Ars-Thong <v.t.nguyen@ar-system.co.jp>
2025-06-03 09:50:54 +0900

    Add Twig Component Bundle

--- app/config/eccube/bundles.php
+++ app/config/eccube/bundles.php
@@ -30,4 +30,5 @@ return [
     Eccube\EccubeBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
