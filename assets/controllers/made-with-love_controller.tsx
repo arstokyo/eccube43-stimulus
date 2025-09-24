e4d604c94c71aaebb57b2396c088f933edc1967c
ArsThong <v.t.nguyen@ar-system.co.jp>
2025-09-24 20:24:13 +0900

    assets/controllersにfetch-lazyを追加

--- assets/controllers/made-with-love_controller.tsx
+++ assets/controllers/made-with-love_controller.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import MadeWithLove from '../components/MadeWithLove';
 import ReactDOM from "react-dom/client";
 
+/* fetch: lazy */
 export default class extends Controller {
     connect() {
         ReactDOM.createRoot(this.element as HTMLElement).render(
